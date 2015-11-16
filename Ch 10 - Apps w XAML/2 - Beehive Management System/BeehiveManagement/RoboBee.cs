06b6fc21142c87180952e7bad0c17702a673c0f1
MarkPThomas <markums@gmail.com>
2015-11-16 09:11:29 -0800

    Completed exercise.

---
+++ Ch 10 - Apps w XAML/2 - Beehive Management System/BeehiveManagement/RoboBee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BeehiveManagement
+{
+    class RoboBee : Robot, IWorker
+    {
+        private int shiftsToWork = 0;
+        private int shiftsWorked = 0;
+        public int ShiftsLeft
+        {
+            get
+            {
+                return shiftsToWork - shiftsWorked;
+            }
+        }
+
+        public string Job { get; private set; }
+
+
+
+        public void DoThisJob(string job, int shifts)
+        {
+            shiftsToWork = shifts;
+            throw new NotImplementedException();
+        }
+
+        public void WorkOneShift()
+        {
+            shiftsWorked++;
+            throw new NotImplementedException();
+        }
+    }
+}
