37711dd75763f792b268c856d8579ba00014a610
MarkPThomas <markums@gmail.com>
2015-11-17 21:46:34 -0800

    Completed exercise.

---
+++ Ch 10 - Apps w XAML/5 - Sloppy Joes - More XAML/SloppyJoes/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SloppyJoes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMakeNewMenu_Click(object sender, RoutedEventArgs e)
+        {
+            // Retrieve the object by setting a temporary variable to reference the XAML static resource
+            MenuMaker menuMaker = FindResource("menuMaker") as MenuMaker;
+            menuMaker.UpdateMenu();
+        }
+    }
+}
---
+++ Ch 10 - Apps w XAML/5 - Sloppy Joes - More XAML/SloppyJoes/MenuItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SloppyJoes
+{
+    public class MenuItem
+    {
+        public string Meat { get; set; }
+        public string Condiment { get; set; }
+        public string Bread { get; set; }
+
+        public MenuItem() { }
+
+        public MenuItem(string meat, string condiment, string bread)
+        {
+            Meat = meat;
+            Condiment = condiment;
+            Bread = bread;
+        }
+
+
+        public override string ToString()
+        {
+            return Meat + " with " + Condiment + " on " + Bread;
+        }
+    }
+}
