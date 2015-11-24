657b98af9f31f6820fe4b659bb5a6c2b1611ef6c
MarkPThomas <markums@gmail.com>
2015-11-24 01:09:58 -0800

    Completed exercise.

---
+++ Ch 13 - The Death of an Object/3 - Dog - Struct/Dog/Canine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dog
+{
+    public class Canine
+    {
+        public string Name;
+        public string Breed;
+
+        public Canine(string name, string breed)
+        {
+            this.Name = name;
+            this.Breed = breed;
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine("My name is {0} and I'm a {1}.", Name, Breed);
+        }
+    }
+}
