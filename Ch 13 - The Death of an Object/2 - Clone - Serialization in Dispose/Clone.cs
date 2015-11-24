14cb85a95b39a8f5a1fa855fc8ea9289ab5e3690
MarkPThomas <markums@gmail.com>
2015-11-24 00:35:38 -0800

    Completed exercise

---
+++ Ch 13 - The Death of an Object/2 - Clone - Serialization in Dispose/Clone.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CloneGarbage
+{
+    [Serializable]
+    class Clone : IDisposable
+    {
+        public int Id { get; private set; }
+
+        public Clone(int Id)
+        {
+            this.Id = Id;
+        }
+
+        // Triggered when called directly, or automatically from 'Using' statements.
+        public void Dispose()
+        {
+            string fileName = @"C:\Temp\Clone.dat";
+            string dirName = @"C:\Temp";
+            if (!File.Exists(fileName))
+            {
+                Directory.CreateDirectory(dirName);
+            }
+            BinaryFormatter bf = new BinaryFormatter();
+            using (Stream output = File.OpenWrite(fileName))
+            {
+                bf.Serialize(output, this);
+            }
+
+            MessageBox.Show("Must...serialize...object!",
+                            "Clone #" + Id + " says...");
+        }
+
+        // Object finalizer. Triggered when garbage collection occurs.
+        ~Clone()
+        {
+            MessageBox.Show("Aaargh! You got me!",
+                            "Clone #" + Id + " says...");
+        }
+    }
+}
---
+++ Ch 13 - The Death of an Object/2 - Clone - Serialization in Dispose/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CloneGarbage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void clone1Button_Click(object sender, EventArgs e)
+        {
+            using (Clone clone1 = new Clone(1))
+            {
+                // Do nothing!
+            }       
+        }
+
+        private void clone2Button_Click(object sender, EventArgs e)
+        {
+            Clone clone2 = new Clone(2);
+            clone2 = null;
+        }
+
+        private void garbageCollect_Click(object sender, EventArgs e)
+        {
+            GC.Collect();
+        }
+    }
+}
