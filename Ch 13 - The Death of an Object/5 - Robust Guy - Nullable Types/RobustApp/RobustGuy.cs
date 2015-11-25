dcd62921060c2e6b9a7976e6c817a072a589231c
MarkPThomas <markums@gmail.com>
2015-11-24 23:58:36 -0800

    Completed exercise.

---
+++ Ch 13 - The Death of an Object/5 - Robust Guy - Nullable Types/RobustApp/RobustGuy.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobustApp
+{
+    class RobustGuy
+    {
+        public DateTime? Birthday { get; private set; }
+        public int? Height { get; private set; }
+
+        public RobustGuy(string birthday, string height)
+        {
+            DateTime tempDate;
+            // TryParse converts tempDate by ref and returns 'true' if successful.
+            if (DateTime.TryParse(birthday, out tempDate))
+            {
+                Birthday = tempDate;
+            }
+            else
+            {
+                Birthday = null;
+            }
+
+            int tempInt;
+            if (int.TryParse(height, out tempInt))
+            {
+                Height = tempInt;
+            }
+            else
+            {
+                Height = null;
+            }
+        }
+
+        // It is a good idea to check for 'HasValue' before any casting or calling '.Value', as the program will throw an exception if the value is null.
+        public override string ToString()
+        {
+            string description;
+            if (Birthday.HasValue)
+            {
+                description = "I was born on " + Birthday.Value.ToLongDateString();
+            }
+            else
+            {
+                description = "I don't know my birthday";
+            }
+
+            if (Height.HasValue)
+            {
+                description += ", and I'm " + Height + " inches tall";
+            }
+            else
+            {
+                description += ", and I don't know my height";
+            }
+
+            return description;
+        }
+    }
+}
