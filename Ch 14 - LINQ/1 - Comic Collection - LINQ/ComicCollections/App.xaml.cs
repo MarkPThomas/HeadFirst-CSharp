48f3b06ae92aee3a39cb49f1642c122bda46d609
MarkPThomas <markums@gmail.com>
2015-12-02 20:18:32 -0800

    Completed exercise

---
+++ Ch 14 - LINQ/1 - Comic Collection - LINQ/ComicCollections/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ComicCollections
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    partial class App : Application
+    {
+
+    }
+}
---
+++ Ch 14 - LINQ/1 - Comic Collection - LINQ/ComicCollections/Comic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicCollections
+{
+    public class Comic
+    {
+        public string Name {get; set;}
+        public int Issue { get; set; }
+
+        
+    }
+}
