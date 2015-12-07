b666baf068ec6bc9c3d3bdcd47bfd338b01e74e4
MarkPThomas <markums@gmail.com>
2015-12-06 20:00:38 -0800

    Completed exercise.

---
+++ Ch 15 - Events and Delegates/4 - Delegate Types/DelegateTypes/ConvertsIntToString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTypes
+{
+    // delegate [method type] DelegateName ([method parameters]);
+    // delegates call methods with matching signatures
+    // method type can be void, string, etc. and only works with a delegate calling a matching type.
+    // method parameters only works with a delegate calling matching parameters to form
+    delegate string ConvertsIntToString(int i);
+
+}
---
+++ Ch 15 - Events and Delegates/4 - Delegate Types/DelegateTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A delegate variable points to a method with a matching signature, instead of a value or object
+            ConvertsIntToString someMethod = new ConvertsIntToString(HiThere);
+
+            // You can set someMethod just like any other variable. 
+            // When you call it like a method, it calls whatever method it happens to point to.
+            string message = someMethod(5);
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+
+        private static string HiThere(int i)
+        {
+            return "Hi there! #" + (i * 100);
+        }
+    }
+}
