a00c2a445c106f08fa5a7e630c70fa1a9b837bac
MarkPThomas <markums@gmail.com>
2015-12-09 16:30:07 -0800

    Completed exercise

---
+++ Ch 16 - MVVM Pattern/1 - Basketball Roster - MVVM/BasketballRoster/View/LeagueWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BasketballRoster.ViewModel;
+
+namespace BasketballRoster.View
+{
+    /// <summary>
+    /// Interaction logic for LeagueWindow.xaml
+    /// </summary>
+    public partial class LeagueWindow : Window
+    {
+        private LeagueViewModel leagueViewModel;
+        public LeagueWindow()
+        {
+            InitializeComponent();
+
+            leagueViewModel = FindResource("LeagueViewModel") as LeagueViewModel;
+        }
+    }
+}
---
+++ Ch 16 - MVVM Pattern/1 - Basketball Roster - MVVM/BasketballRoster/ViewModel/RosterViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasketballRoster.Model;
+
+namespace BasketballRoster.ViewModel
+{
+    class RosterViewModel
+    {
+        public string TeamName { get; set; }
+        public ObservableCollection<PlayerViewModel> Starters {get; set; }
+        public ObservableCollection<PlayerViewModel> Bench { get; set; }
+
+        private Roster _roster;
+
+        public RosterViewModel(Roster roster)
+        {
+            _roster = roster;
+
+            Starters = new ObservableCollection<PlayerViewModel>();
+            Bench = new ObservableCollection<PlayerViewModel>();
+
+            TeamName = roster.TeamName;
+            UpdateRosters();
+        }
+
+        private void UpdateRosters()
+        {
+            // Uses LINQ queries to extract the starting and bench players
+            // And update the Starters & Bench properties
+            var startingPlayers = from player in _roster.Players
+                                  where player.Starter
+                                  select player;
+
+            foreach (Player player in startingPlayers)
+            {
+                Starters.Add(new PlayerViewModel(player.Name, player.Number));
+            }
+
+            var benchPlayers = from player in _roster.Players
+                               where player.Starter == false
+                               select player;
+
+            foreach (Player player in benchPlayers)
+            {
+                Bench.Add(new PlayerViewModel(player.Name, player.Number));
+            }
+        }
+    }
+}
