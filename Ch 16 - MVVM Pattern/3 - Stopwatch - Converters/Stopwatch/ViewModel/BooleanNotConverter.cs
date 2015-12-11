6507d7594254bdff1ebf4ef3b91e50ca96081500
MarkPThomas <markums@gmail.com>
2015-12-10 22:17:20 -0800

    Completed exercise.

---
+++ Ch 16 - MVVM Pattern/3 - Stopwatch - Converters/Stopwatch/ViewModel/BooleanNotConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Stopwatch.ViewModel
+{
+    class BooleanNotConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((value is bool) && ((bool)value) == false)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
