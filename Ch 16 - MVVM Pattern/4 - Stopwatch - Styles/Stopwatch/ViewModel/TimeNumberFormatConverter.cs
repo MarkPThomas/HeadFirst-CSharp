a3a3bb8d3f2ff49abb5d2cf1daf8a089cb36f85c
MarkPThomas <markums@gmail.com>
2015-12-10 22:30:51 -0800

    Completed exercise.

---
+++ Ch 16 - MVVM Pattern/4 - Stopwatch - Styles/Stopwatch/ViewModel/TimeNumberFormatConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Stopwatch.ViewModel
+{
+    class TimeNumberFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal)
+            {
+                return ((decimal)value).ToString("00.00");
+            }
+            else if (value is int)
+            {
+                if (parameter == null)
+                {
+                    return ((int)value).ToString("d1");
+                }
+                else
+                {
+                    return ((int)value).ToString(parameter.ToString());
+                }
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
