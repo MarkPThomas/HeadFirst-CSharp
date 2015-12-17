33ebf6aa3b13c07f53c3eb03a6d84fe3aa66ed0c
MarkPThomas <markums@gmail.com>
2015-12-16 20:56:06 -0800

    Completed exercise.

---
+++ Ch Appendix i - Leftovers/6 - Equality, IEquatable & Equals/LeftOver6/EquatableGuyWithOverload.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeftOver6
+{
+    /// <summary>
+    /// A guy that knows how to compare itself with other guys.
+    /// </summary>
+    class EquatableGuyWithOverload : EquatableGuy
+    {
+        public EquatableGuyWithOverload(string name, int age, int cash)
+            : base(name, age, cash)
+        { }
+
+        public static bool operator == (EquatableGuyWithOverload left,
+                                        EquatableGuyWithOverload right)
+        {
+            // Comparison to null now needs to be done this way to avoid a StackOverflowException
+            if (ReferenceEquals(left, null))
+            {
+                return false;
+            }
+            else
+            {
+                return left.Equals(right);
+            }
+        }
+
+        public static bool operator != (EquatableGuyWithOverload left,
+                                        EquatableGuyWithOverload right)
+        {
+            return !(left == right);
+        }
+
+        // The methods below must also be overwritten
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ Ch Appendix i - Leftovers/6 - Equality, IEquatable & Equals/LeftOver6/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeftOver6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guy joe1 = new Guy("Joe", 37, 100);
+
+            // True
+            Guy joe2 = joe1;
+            Console.WriteLine(ReferenceEquals(joe1, joe2));
+            Console.WriteLine(joe1.Equals(joe2));
+            Console.WriteLine(ReferenceEquals(null, null));
+
+            // False
+            joe2 = new Guy("Joe", 37, 100);
+            Console.WriteLine(ReferenceEquals(joe1, joe2));
+            Console.WriteLine(joe1.Equals(joe2));
+
+            // Now that Equals() and GetHashCode() are implemented in EquatableGuy, the following will work:
+            List<Guy> guys = new List<Guy>()
+            {
+                new Guy("Bob", 42, 125),
+                new EquatableGuy(joe1.Name, joe1.Age, joe1.Cash),
+                new Guy("ed", 39, 95)
+            };
+
+            // True
+            Console.WriteLine(guys.Contains(joe1));
+
+            // False
+            Console.WriteLine(joe1 == joe2);
+
+            // Object comparison with overwritten equals operators
+            joe1 = new EquatableGuyWithOverload(joe1.Name, joe1.Age, joe1.Cash);
+            joe2 = new EquatableGuyWithOverload(joe1.Name, joe1.Age, joe1.Cash);
+
+            // Below does not work as expected beause its calling Guy's == & != operators
+            // False
+            Console.WriteLine(joe1 == joe2);
+            // True
+            Console.WriteLine(joe1 != joe2);
+
+            // Cast to EquatableGuyWithOverload to call the correct == & !=
+            // True
+            Console.WriteLine((EquatableGuyWithOverload)joe1 == (EquatableGuyWithOverload)joe2);
+            // False
+            Console.WriteLine((EquatableGuyWithOverload)joe1 != (EquatableGuyWithOverload)joe2);
+
+            joe2.ReceiveCash(25);
+
+            // False
+            Console.WriteLine((EquatableGuyWithOverload)joe1 == (EquatableGuyWithOverload)joe2);
+            // True
+            Console.WriteLine((EquatableGuyWithOverload)joe1 != (EquatableGuyWithOverload)joe2);
+
+            // Now to make it work without casting
+            EquatableGuyWithOverload joe3 = new EquatableGuyWithOverload(joe1.Name, joe1.Age, joe1.Cash);
+            EquatableGuyWithOverload joe4 = new EquatableGuyWithOverload(joe1.Name, joe1.Age, joe1.Cash);
+
+            // True
+            Console.WriteLine(joe3 == joe4);
+            // False
+            Console.WriteLine(joe3 != joe4);
+
+            Console.ReadKey();
+        }
+    }
+}
